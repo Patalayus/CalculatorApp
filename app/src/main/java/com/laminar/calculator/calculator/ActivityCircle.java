521c829e5ec24b5388dd6a6bbdf6ab3a547406ff
Luke Charles <lukecharles222@gmail.com>
2017-08-30 23:09:23 +0100

    completed area of a circle calculation

--- app/src/main/java/com/laminar/calculator/calculator/ActivityCircle.java
+++ app/src/main/java/com/laminar/calculator/calculator/ActivityCircle.java
@@ -2,6 +2,9 @@ package com.laminar.calculator.calculator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -11,5 +14,21 @@ public class ActivityCircle extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_circle);
-}}
+        setContentView(R.layout.activity_circle);}
+
+
+        public void RadiusCalculation(View a){
+
+            if(a.getId() == R.id.calculateButton_ID){
+                EditText RadiusVariable = (EditText)findViewById(R.id.radius_text);
+                double ConvertedR = Double.parseDouble(RadiusVariable.getText().toString());
+
+                double RadHalf = ConvertedR * ConvertedR;
+                double RadComp = Math.PI * RadHalf;
+
+                TextView TextViewV = (TextView)findViewById(R.id.output_text);
+                TextViewV.setText(""+RadComp);
+            }
+    }
+
+}
