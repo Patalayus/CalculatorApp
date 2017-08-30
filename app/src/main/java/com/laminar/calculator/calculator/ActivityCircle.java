82e53fbf2da3188a54e618391735b327753c2e5a
Luke Charles <lukecharles222@gmail.com>
2017-08-30 23:32:40 +0100

    image view

--- app/src/main/java/com/laminar/calculator/calculator/ActivityCircle.java
+++ app/src/main/java/com/laminar/calculator/calculator/ActivityCircle.java
@@ -2,6 +2,7 @@ package com.laminar.calculator.calculator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -12,23 +13,22 @@ public class ActivityCircle extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_circle);}
 
-
         public void RadiusCalculation(View a){
-
-            if(a.getId() == R.id.calculateButton_ID){
-                EditText RadiusVariable = (EditText)findViewById(R.id.radius_text);
+            EditText RadiusVariable = (EditText)findViewById(R.id.radius_text);
+            if(RadiusVariable.length()==0){
+                Snackbar Reminder = Snackbar.make(a, "Please enter a number", Snackbar.LENGTH_LONG);
+                Reminder.show();
+            }else if (a.getId() == R.id.calculateButton_ID)
+            {
                 double ConvertedR = Double.parseDouble(RadiusVariable.getText().toString());
-
                 double RadHalf = ConvertedR * ConvertedR;
                 double RadComp = Math.PI * RadHalf;
-
                 TextView TextViewV = (TextView)findViewById(R.id.output_text);
                 TextViewV.setText(""+RadComp);
+                RadiusVariable.setText("");
             }
     }
-
 }
