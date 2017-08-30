bd23d0452e2ae9b2084a3dca076097a8b7cf9912
David <davmatjo@gmail.com>
2017-08-30 13:29:21 +0100

    Pressing done now also calculates

--- app/src/main/java/com/laminar/calculator/calculator/MainActivity.java
+++ app/src/main/java/com/laminar/calculator/calculator/MainActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -228,6 +230,16 @@ public class MainActivity extends AppCompatActivity {
                     result.setText(String.valueOf(enps_result));
                     current_op = 0;
                 }}
+        });
+        Number1.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    calculate.performClick();
+                    return true;
+                }
+                return false;
+            }
         });
 
     }
