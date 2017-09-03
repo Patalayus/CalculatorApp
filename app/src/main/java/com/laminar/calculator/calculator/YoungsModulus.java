ef7ce22c3353d64c87c85abde9cd16ac2eee2561
Luke Charles <lukecharles222@gmail.com>
2017-09-03 20:04:19 +0100

    edit text for Young's Modulus input accepts double values

--- app/src/main/java/com/laminar/calculator/calculator/YoungsModulus.java
+++ app/src/main/java/com/laminar/calculator/calculator/YoungsModulus.java
@@ -13,20 +13,28 @@ import android.widget.TextView;
 
 public class YoungsModulus extends AppCompatActivity {
 
+    EditText force, area, changelength, length;
+    TextView enps_text;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_youngs_modulus);
-    }
-    public void YoungsCalculation(View a){
-        double force_v, area_v, change_v, length_v, youngs;
-
-        EditText force, area, changelength, length;
 
         force = (EditText)findViewById(R.id.forceid);
         area = (EditText)findViewById(R.id.A_ID);
         changelength = (EditText)findViewById(R.id.DeltaL_ID);
         length = (EditText)findViewById(R.id.L_ID);
+        enps_text = (TextView)findViewById(R.id.bracket);
+
+        force.setHint("Enter your force in newtons (N)");
+        area.setHint("Enter your area in square meters (m^2)");
+        changelength.setHint("Enter your change in length in meters (m)");
+        length.setHint("Enter your original length in meters (m)");
+        enps_text.setText(" Work out the Young's Modulus of an object.");
+    }
+    public void YoungsCalculation(View a){
+        double force_v, area_v, change_v, length_v, youngs;
 
         if(force.length()==0||area.length()==0||changelength.length()==0||length.length()==0){
             Snackbar snackbar = Snackbar.make(a, "Please enter 3 numbers", Snackbar.LENGTH_LONG);
@@ -34,9 +42,9 @@ public class YoungsModulus extends AppCompatActivity {
         }
         else if(a.getId() == R.id.calculation){
             TextView outputtext = (TextView)findViewById(R.id.idofoutput);
-            youngs = (Double.parseDouble(force.getText().toString())/Double.parseDouble(area.getText().toString()))
+            youngs = ( Double.parseDouble(force.getText().toString())/Double.parseDouble(area.getText().toString()))
                     /(Double.parseDouble(changelength.getText().toString())/Double.parseDouble(length.getText().toString()));
-            outputtext.setText("Young's Modulus: "+youngs);
+            outputtext.setText("Young's Modulus: "+youngs+" Pascals");
         }
     }
 }
