be142dec658ee4fb2a67f221503bd244f227ed9c
Luke Charles <lukecharles222@gmail.com>
2017-09-06 11:14:42 +0100

    button functions added to suvat activity

--- app/src/main/java/com/laminar/calculator/calculator/activity_suvat.java
+++ app/src/main/java/com/laminar/calculator/calculator/activity_suvat.java
@@ -1,13 +1,40 @@
 package com.laminar.calculator.calculator;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class activity_suvat extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_suvat);
+        setContentView(R.layout.activity_suvat);}
+
+    public void v_uat(View a) {
+        //when first suvat button is pressed
     }
-}
+
+    public void sutat(View a) {
+        //when first suvat button is pressed
+    }
+
+    public void suvt(View a) {
+        //when first suvat button is pressed
+    }
+
+    public void vuas(View a) {
+        //when first suvat button is pressed
+    }
+
+    public void svtat(View a) {
+        //when first suvat button is pressed
+    }
+
+
+    }
+
+
