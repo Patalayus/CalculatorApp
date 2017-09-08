269d80575079d460b2732707301233161515851d
Luke Charles <lukecharles222@gmail.com>
2017-09-08 14:35:56 +0100

    fixed activity code

--- app/src/main/java/com/laminar/calculator/calculator/sutat_activity.java
+++ app/src/main/java/com/laminar/calculator/calculator/sutat_activity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class sutat_activity extends AppCompatActivity {
 
@@ -20,10 +22,32 @@ public class sutat_activity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "substitute values into the equation", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
-    }
+        });}
+    public void onPressExecute(View a){
+        EditText initial_id1 = (EditText)findViewById(R.id.initialvelocityid);
+        EditText area_id1 = (EditText)findViewById(R.id.area2id);
+        EditText time_id1 = (EditText)findViewById(R.id.time2is);
 
+        double enps_InitialVelocity1 = Double.parseDouble(initial_id1.getText().toString());
+        double enps_areaid1 = Double.parseDouble(area_id1.getText().toString());
+        double enps_time1id = Double.parseDouble(time_id1.getText().toString());
+
+        if(initial_id1.length()==0||area_id1.length()==0||time_id1.length()==0){
+            Snackbar.make(a, "Please enter your values into the equation", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        }else{
+            //S =U * T + 1/2 * AT^2
+            double enps_UmultT = enps_InitialVelocity1 * enps_time1id;
+            double enps_end = (enps_areaid1*enps_time1id)*(enps_areaid1*enps_time1id);
+            double enps_total22 = enps_UmultT+0.5*enps_end;
+            TextView enps_total222 = (TextView)findViewById(R.id.outputtextshsh);
+            enps_total222.setText("Your total is "+enps_total22);
+
+        }
+    }
 }
+
+
--- app/src/main/java/com/laminar/calculator/calculator/svtat_activity.java
+++ app/src/main/java/com/laminar/calculator/calculator/svtat_activity.java
@@ -26,27 +26,7 @@ public class svtat_activity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });}
-        public void finalvelocity_ID(View a){
-            EditText initial_id1 = (EditText)findViewById(R.id.initialvelocityid);
-            EditText area_id1 = (EditText)findViewById(R.id.area2id);
-            EditText time_id1 = (EditText)findViewById(R.id.time2is);
 
-            if(initial_id1.length()==0||area_id1.length()==0||time_id1.length()==0){
-                Snackbar.make(a, "Please enter your values into the equation", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }else{
-                double enps_InitialVelocity1 = Double.parseDouble(initial_id1.getText().toString());
-                double enps_areaid1 = Double.parseDouble(area_id1.getText().toString());
-                double enps_time1id = Double.parseDouble(time_id1.getText().toString());
-                //S =U * T + 1/2 * AT^2
-                double enps_UmultT = enps_InitialVelocity1 * enps_time1id;
-                double enps_end = (enps_areaid1*enps_time1id)*(enps_areaid1*enps_time1id);
-                double enps_total22 = enps_UmultT+0.5*enps_end;
-                TextView enps_total222 = (TextView)findViewById(R.id.output_total22);
-                enps_total222.setText("Your total is "+enps_total22);
-
-            }
-        }
 
     }
 
