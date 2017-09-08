4671f60914fdb7f0b4ed06107de6c7f93736047c
Luke Charles <lukecharles222@gmail.com>
2017-09-08 12:31:14 +0100

    suvat activity two altered

--- app/src/main/java/com/laminar/calculator/calculator/svtat_activity.java
+++ app/src/main/java/com/laminar/calculator/calculator/svtat_activity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class svtat_activity extends AppCompatActivity {
 
@@ -20,10 +21,22 @@ public class svtat_activity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Please enter your values into the equation", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });}
+        public void finalvelocity_ID(View a){
+            EditText initial_id1 = (EditText)findViewById(R.id.initialvelocityid);
+            EditText area_id1 = (EditText)findViewById(R.id.area2id);
+            EditText time_id1 = (EditText)findViewById(R.id.time2is);
+
+            if(initial_id1.length()==0||area_id1.length()==0||time_id1.length()==0){
+                Snackbar.make(a, "Please enter your values into the equation", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }else{
+
+            }
+        }
+
     }
 
-}
