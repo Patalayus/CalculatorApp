06c2231fe0f1765986b000f5b15554b74ef04b44
Luke Charles <lukecharles222@gmail.com>
2017-09-09 13:00:18 +0100

    final activity finished

--- app/src/main/java/com/laminar/calculator/calculator/svtat_activity.java
+++ app/src/main/java/com/laminar/calculator/calculator/svtat_activity.java
@@ -26,7 +26,30 @@ public class svtat_activity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });}
+    public void AllWorkAndNoPlay(View a){
+        EditText finalvel = (EditText)findViewById(R.id.finalvelo);
+        EditText time = (EditText)findViewById(R.id.timea);
+        EditText area = (EditText)findViewById(R.id.areaz);
 
+        double finalvel2 = Double.parseDouble(finalvel.getText().toString());
+        double time1 = Double.parseDouble(time.getText().toString());
+        double area1 = Double.parseDouble(area.getText().toString());
 
+        if(finalvel.length()==0||time.length()==0||area.length()==0){
+            Snackbar.make(a, "Please enter your values into the equation", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        }else{
+            //S = V T - 1/2 * A T ^2
+            double enps_VT = finalvel2 * time1;
+            double enps_AT2 = (area1*time1)*(area1*time1);
+            double enps_rest = 0.5*enps_AT2;
+
+            double variabletotal = enps_VT - enps_rest;
+
+            TextView enps_total4all = (TextView)findViewById(R.id.textView10);
+            enps_total4all.setText("Your total is "+variabletotal+"m");
+
+        }
     }
+}
 
