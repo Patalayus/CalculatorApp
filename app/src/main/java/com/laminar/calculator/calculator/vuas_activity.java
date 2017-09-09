202be9bd3ff190dbe3972499731dc882602d6cbd
Luke Charles <lukecharles222@gmail.com>
2017-09-09 12:35:45 +0100

    V^2=U^2+2*AS activity finished

--- app/src/main/java/com/laminar/calculator/calculator/vuas_activity.java
+++ app/src/main/java/com/laminar/calculator/calculator/vuas_activity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class vuas_activity extends AppCompatActivity {
 
@@ -20,10 +22,32 @@ public class vuas_activity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Substitute values into the SUVAT equation provided", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });}
+    public void onPressWorkThrough(View a){
+        EditText initial_id1 = (EditText)findViewById(R.id.velocity1);
+        EditText area_id1 = (EditText)findViewById(R.id.area1);
+        EditText time_id1 = (EditText)findViewById(R.id.time1);
+
+        double enps_InitialVelocity1 = Double.parseDouble(initial_id1.getText().toString());
+        double enps_areaid1 = Double.parseDouble(area_id1.getText().toString());
+        double enps_time1id = Double.parseDouble(time_id1.getText().toString());
+
+        if(initial_id1.length()==0||area_id1.length()==0||time_id1.length()==0){
+            Snackbar.make(a, "Please enter your values into the equation", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        }else{
+            //V ^2 = U ^2 + 2 * A S
+            double enps_2AS = 2*(enps_areaid1*enps_time1id);
+            double enps_U2 = enps_InitialVelocity1*enps_InitialVelocity1;
+            double allwork = enps_U2 + enps_2AS;
+            TextView asdasda = (TextView)findViewById(R.id.allthroughtext);
+            asdasda.setText("Your total is "+allwork+"m^2");
+
+        }
+    }
     }
 
-}
+
