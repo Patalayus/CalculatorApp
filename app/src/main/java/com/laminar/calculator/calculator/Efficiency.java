623ba8f347efee8dbc85fedda50963ca2a99f771
Luke Charles <lukecharles222@gmail.com>
2018-04-02 18:14:32 +0100

    added efficiency activity

--- app/src/main/java/com/laminar/calculator/calculator/Efficiency.java
+++ app/src/main/java/com/laminar/calculator/calculator/Efficiency.java
@@ -1,7 +1,11 @@
 package com.laminar.calculator.calculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Efficiency extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class Efficiency extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_efficiency);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
     }
+    public void workout(View a){
+        EditText EO = (EditText)findViewById(R.id.energyoutput);
+        EditText EI = (EditText)findViewById(R.id.energyinput);
+        TextView VIEW = (TextView)findViewById(R.id.textView15);
+
+        if(EO.length()==0||EI.length()==0){
+            Snackbar.make(a, "Please enter your values into the equation", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        }else{
+
+            double oe = Double.parseDouble(EO.getText().toString());
+            double ei = Double.parseDouble(EI.getText().toString());
+            double stage1 = oe / ei;
+            double stage2 = stage1 * 100;
+
+            VIEW.setText("Efficiency: "+stage2+"J");
+        }
+    }
+
 }
