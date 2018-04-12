3bffe8e2b72c3e80f0aacfc5af3c2f602025120c
Luke Charles <lukecharles222@gmail.com>
2018-04-12 18:11:12 +0100

    updated voltage activity

--- app/src/main/java/com/laminar/calculator/calculator/voltage.java
+++ app/src/main/java/com/laminar/calculator/calculator/voltage.java
@@ -1,7 +1,11 @@
 package com.laminar.calculator.calculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class voltage extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class voltage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voltage);
+        //V = W / A
+    }
+    public void dovoltage(View a){
+        TextView answer = (TextView)findViewById(R.id.answerID);
+        EditText wattage = (EditText)findViewById(R.id.wattageID);
+        EditText amperage = (EditText)findViewById(R.id.amperageID);
+
+        if(wattage.length()==0||amperage.length()==0){
+            Snackbar.make(a, "Please enter your values into the equation", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        }else{
+            double conamperage = Double.parseDouble(amperage.getText().toString());
+            double conwattage = Double.parseDouble(wattage.getText().toString());
+
+            double total = conwattage / conamperage;
+            answer.setText("Your total is "+total+"V");
+
+        }
     }
 }
