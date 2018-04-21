855225ad7835dcc3fde076e229e244143aef9153
Luke Charles <lukecharles222@gmail.com>
2018-04-21 12:28:26 +0100

    periodic time activity added

---
+++ app/src/main/java/com/laminar/calculator/calculator/periodic_time.java
@@ -0,0 +1,13 @@
+package com.laminar.calculator.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class periodic_time extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_periodic_time);
+    }
+}
