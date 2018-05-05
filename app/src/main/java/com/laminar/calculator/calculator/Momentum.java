7db7085f64465e39b98a6fc8226ad35d712526f3
Luke Charles <lukecharles222@gmail.com>
2018-05-05 15:52:18 +0100

    initial method added

--- app/src/main/java/com/laminar/calculator/calculator/Momentum.java
+++ app/src/main/java/com/laminar/calculator/calculator/Momentum.java
@@ -1,12 +1,8 @@
 package com.laminar.calculator.calculator;
 
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class Momentum extends AppCompatActivity {
 
@@ -16,21 +12,7 @@ public class Momentum extends AppCompatActivity {
         setContentView(R.layout.activity_momentum);
     }
     public void work(View a){
-        EditText mass = (EditText)findViewById(R.id.mass);
-        EditText velocity = (EditText)findViewById(R.id.velocity);
-        Button calculate = (Button)findViewById(R.id.calculate);
-        TextView answer = (TextView)findViewById(R.id.answer);
-        if(mass.length()==0||velocity.length()==0){
-            Snackbar.make(a, "Please enter your values into the equation", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show();
-        }else{
-            //P = M * V
-            double massCON = Double.parseDouble(mass.getText().toString());
-            double velocityCON = Double.parseDouble(velocity.getText().toString());
-            double answer_total = massCON * velocityCON;
-            answer.setText("Your momentum is "+answer_total+"kgm/s");
 
-        }
     }
 }
 
