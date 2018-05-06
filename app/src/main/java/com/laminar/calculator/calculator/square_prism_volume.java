2d9d8d0344d869965f57d3d2fd0f4aea1ec6127a
Luke Charles <lukecharles222@gmail.com>
2018-05-06 13:23:38 +0100

    activities for prisms added

---
+++ app/src/main/java/com/laminar/calculator/calculator/square_prism_volume.java
@@ -0,0 +1,13 @@
+package com.laminar.calculator.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class square_prism_volume extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_square_prism_volume);
+    }
+}
