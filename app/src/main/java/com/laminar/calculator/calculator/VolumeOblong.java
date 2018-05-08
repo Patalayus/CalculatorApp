a89df3135d1dedd574fb2bf16da3f3ec4f9a0b2c
Luke Charles <lukecharles222@gmail.com>
2018-05-08 21:49:44 +0100

    volume of prism activity .java

--- app/src/main/java/com/laminar/calculator/calculator/VolumeOblong.java
+++ app/src/main/java/com/laminar/calculator/calculator/VolumeOblong.java
@@ -1,35 +1,50 @@
 package com.laminar.calculator.calculator;
 
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class VolumeOblong extends AppCompatActivity {
 
+    Button cylinder, squarebased_prism, triangularbased_prism, octagonalbased_prism;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_volume_oblong);
-    }
-    public void work(View a){
-        EditText mass = (EditText)findViewById(R.id.mass);
-        EditText velocity = (EditText)findViewById(R.id.velocity);
-        Button calculate = (Button)findViewById(R.id.calculate);
-        TextView answer = (TextView)findViewById(R.id.answer);
-        if(mass.length()==0||velocity.length()==0){
-            Snackbar.make(a, "Please enter your values into the equation", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show();
-        }else{
-            //P = M * V
-            double massCON = Double.parseDouble(mass.getText().toString());
-            double velocityCON = Double.parseDouble(velocity.getText().toString());
-            double answer_total = massCON * velocityCON;
-            answer.setText("Your momentum is "+answer_total+"kgm/s");
 
+        cylinder = (Button)findViewById(R.id.button4);
+        squarebased_prism = (Button)findViewById(R.id.squarebasedID);
+        triangularbased_prism = (Button)findViewById(R.id.triangularbasedID);
+        octagonalbased_prism = (Button)findViewById(R.id.octagonalbasedID);
+    }
+    public void gocylinder(View v){
+        if(v.getId()==R.id.button4){
+            Intent gocylinder = new Intent(VolumeOblong.this, cylinder_volume.class);
+            startActivity(gocylinder);
         }
+
+    }
+    public void gosquareprism(View v){
+        if(v.getId()==R.id.squarebasedID){
+            Intent gosquarebased = new Intent(VolumeOblong.this, cylinder_volume.class);
+            startActivity(gosquarebased);
+        }
+
+    }
+    public void gotriangleprism(View v){
+        if(v.getId()==R.id.triangularbasedID){
+            Intent gotriangular = new Intent(VolumeOblong.this, cylinder_volume.class);
+            startActivity(gotriangular);
+        }
+
+    }
+    public void gooctagonalprism(View v){
+        if(v.getId()==R.id.octagonalbasedID){
+            Intent gooctagonal = new Intent(VolumeOblong.this, cylinder_volume.class);
+            startActivity(gooctagonal);
+        }
+
     }
 }
