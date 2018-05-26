33f211c2d179cce75c3f0f903115d9f40bb65fe1
Luke Charles <lukecharles222@gmail.com>
2018-05-26 22:51:59 +0100

    layout fix triangular prism

--- app/src/main/java/com/laminar/calculator/calculator/triangular_prism_volume.java
+++ app/src/main/java/com/laminar/calculator/calculator/triangular_prism_volume.java
@@ -9,6 +9,5 @@ public class triangular_prism_volume extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_triangular_prism_volume);
-
-    }
+        }
 }
