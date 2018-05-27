3e3de08915b1e87f4350c34e0b879a69064ab584
Luke Charles <lukecharles222@gmail.com>
2018-05-27 14:01:02 +0100

    activity code change

--- app/src/main/java/com/laminar/calculator/calculator/triangular_prism_volume.java
+++ app/src/main/java/com/laminar/calculator/calculator/triangular_prism_volume.java
@@ -9,5 +9,6 @@ public class triangular_prism_volume extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_triangular_prism_volume);
+
         }
 }
