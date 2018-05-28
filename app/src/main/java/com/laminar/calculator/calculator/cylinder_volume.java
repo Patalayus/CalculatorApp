84f646a83d0f0fc8d419c514d8e0a511f0c9a0f4
Luke Charles <lukecharles222@gmail.com>
2018-05-28 12:29:26 +0100

    function part 1 added

--- app/src/main/java/com/laminar/calculator/calculator/cylinder_volume.java
+++ app/src/main/java/com/laminar/calculator/calculator/cylinder_volume.java
@@ -1,7 +1,11 @@
 package com.laminar.calculator.calculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class cylinder_volume extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class cylinder_volume extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cylinder_volume);
+    }
+    public void work(View a){
+        EditText radius = (EditText)findViewById(R.id.radiusID);
+        EditText length = (EditText)findViewById(R.id.lengthID);
+
+        if(radius.length()==0||length.length()==0){
+            Snackbar.make(a, "Please enter your values into the equation", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        }else{
+            // Volume of cylinder = Length * (3.141592654 * R^2)
+            double radCON = Double.parseDouble(radius.getText().toString());
+            double lenCON = Double.parseDouble(length.getText().toString());
+
+
+            double answer_total = WidthCON * HeightCON * LenCON;
+            answer.setText("Your volume is "+answer_total+"m^3");
+        }
     }
 }
