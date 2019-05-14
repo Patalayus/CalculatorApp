6e8ab26bc791e24e54287c8e8892b482210fed50
Luke Charles <lukecharles222@gmail.com>
2019-05-14 14:58:51 +0100

    hypotenuse implemented

--- app/src/main/java/com/laminar/calculator/calculator/hypotenuse.java
+++ app/src/main/java/com/laminar/calculator/calculator/hypotenuse.java
@@ -1,7 +1,11 @@
 package com.laminar.calculator.calculator;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class hypotenuse extends AppCompatActivity {
 
@@ -9,5 +13,31 @@ public class hypotenuse extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hypotenuse);
+    }
+
+    public void calculateHyp(View a) {
+        EditText opposite = (EditText) findViewById(R.id.editText_opposite);
+        EditText adjacent = (EditText)findViewById(R.id.editText_adjacent);
+        TextView answerTextView = (TextView)findViewById(R.id.textView_answerID);
+
+        if(opposite.length()==0||adjacent.length()==0){
+            Snackbar.make(a, "Please enter your values into the equation", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        }else{
+            //convert the text to a double format so math can be applied
+            double totOpp = Double.parseDouble(opposite.getText().toString());
+            double totAdj = Double.parseDouble(adjacent.getText().toString());
+
+            //complete application calculation here
+            //a^2 + b^2 = c^2
+
+            double totOppSquared = totOpp*totOpp;
+            double totAjdSquared = totAdj*totAdj;
+            double hypTotal = totAjdSquared + totOppSquared;
+
+            //output answer
+            answerTextView.setText("Your hypotenuse is "+hypTotal);
+        }
+
     }
 }
--- app/src/main/java/com/laminar/calculator/calculator/pythagoras_activity.java
+++ app/src/main/java/com/laminar/calculator/calculator/pythagoras_activity.java
@@ -1,7 +1,9 @@
 package com.laminar.calculator.calculator;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class pythagoras_activity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class pythagoras_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pythagoras_activity);
+    }
+
+    public void loadHypotenuse(View a) {
+        //when hypotenuse button is pressed
+        if(a.getId()==R.id.hypotenuseID){
+            Intent hypotenuse_Intent = new Intent(pythagoras_activity.this, hypotenuse.class);
+            startActivity(hypotenuse_Intent);
+        }
     }
 }
